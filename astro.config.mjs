8e59c72cc7c4dadba5c699bc807fc60b473b796f
Francois <fsherwood@ebi.ac.uk>
2024-10-16 14:07:02 +0100

    switch to react

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,14 @@
 import { defineConfig } from 'astro/config';
 import mdx from "@astrojs/mdx";
 import yaml from '@rollup/plugin-yaml';
-import preact from "@astrojs/preact";
+import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
   //output: 'hybrid',
   integrations: [
     mdx(),
-    preact()
+    react()
   ],
   outDir: './dist/bioimage-archive',
   vite: {
--- src/components/ImageDatasetInfo.tsx
+++ src/components/ImageDatasetInfo.tsx
@@ -1,10 +1,67 @@
-import preact from 'preact';
+import React, { useEffect, useState } from 'react';
+
+
+const ImageDatasetInfo = (props) => {
+    const [data, setData] = useState([]);
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState(null);
+
+    useEffect(() => {
+        fetchData();
+      }, []);
+    
+    const fetchData = async () => {
+        try {
+          const url = 'https://wwwdev.ebi.ac.uk/bioimage-archive/api/v2/experimental_imaging_dataset/' + props.uuid
+          const response = await fetch(url, {method: 'GET', mode:"no-cors", headers: {"Content-Type": "application/json"}}); // Replace with your API endpoint
+          if (!response.ok) {
+            throw new Error('Network response was not ok.');
+          }
+          const data = await response.json();
+          setData(data);
+          setLoading(false);
+        } catch (error) {
+          setError(error.message);
+          setLoading(false);
+        }
+      };
+      if (loading) {
+        return (
+            <section className="vf-content | embl-grid | embl-grid--has-centered-content">
+                <div>
+                    <h3>Dataset</h3>
+                </div>
+                <div>
+                    No Data
+                </div>
+            </section> 
+
+        )
+        } else if (error) {
+            return (
+                <section className="vf-content | embl-grid | embl-grid--has-centered-content">
+                    <div>
+                        <h3>Dataset</h3>
+                    </div>
+                    <div>
+                        {error}
+                    </div>
+                </section> 
+            )
+      } else {
+        console.log(data)
+        return FormatDataset(data)
+      }
+      
+  };
+
+
 
 
 function FormatDataset(dataset) {
-    //console.log(dataset)
+    console.log(dataset)
     return (
-    <section class="vf-content | embl-grid embl-grid--has-centered-content">
+    <section className="vf-content | embl-grid | embl-grid--has-centered-content">
             <div>
                 <h3>Dataset</h3>
             </div>
@@ -16,21 +73,4 @@ function FormatDataset(dataset) {
     );
 }
 
-export default class ImageDatasetInfo extends preact.Component {
-    
-    constructor(props) {
-        super(props);
-    }
-
-    dataset_url = "https://wwwdev.ebi.ac.uk/bioimage-archive/api/v2/experimental_imaging_dataset/" + this.props.uuid
-    
-
-    makeRequest(url) { return fetch(url) .then(response => response.json());}
-
-
-
-    render() {
-        return this.makeRequest(this.dataset_url).then(data => {FormatDataset(data)})
-    }
-}
-
+export default ImageDatasetInfo;
