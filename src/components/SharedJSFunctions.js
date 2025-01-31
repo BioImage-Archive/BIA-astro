e428a9e23ed03ca11e3b2967462f3773d3e754a6
Francois <fsherwood@ebi.ac.uk>
2025-01-31 17:09:15 +0000

    remove search bar, fix alignment, and use string templates for function

--- src/components/SharedJSFunctions.js
+++ src/components/SharedJSFunctions.js
@@ -33,5 +33,5 @@ export function multiline_text_render(value) {
 
 export function humanFileSize(size) {
     var i = size == 0 ? 0 : Math.floor(Math.log(size) / Math.log(1024));
-    return +((size / Math.pow(1024, i)).toFixed(2)) * 1 + ' ' + ['B', 'kB', 'MB', 'GB', 'TB'][i];
+    return `${Number(size / Math.pow(1024, i)).toFixed(2)} ${['B', 'kB', 'MB', 'GB', 'TB', 'PB'][i]}`
 }
