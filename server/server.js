eb08046ef96bb7fe36b942a1ac1de82999962950
Liviu Anita <liviu@ebi.ac.uk>
2025-06-18 11:00:39 +0100

    Delete server

--- server/server.js
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-import { handler as ssrHandler } from '../dist/bioimage-archive/server/entry.mjs';
-
-const app = express();
-const base = '/bioimage-archive';
-app.use(base, express.static('dist/bioimage-archive/client/'));
-app.use(ssrHandler);
-
-app.listen(8080);
