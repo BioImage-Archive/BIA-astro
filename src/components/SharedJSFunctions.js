4dfd3121b363eeb1499e95eba03c00d68e4fe4f6
Tushar <tushar@ebi.ac.uk>
2025-11-28 16:03:37 +0000

    BigStudy Fixes

--- src/components/SharedJSFunctions.js
+++ src/components/SharedJSFunctions.js
@@ -266,3 +266,23 @@ export function getTutorialURLs(urlType){
     const quickTourURL = "https://www.ebi.ac.uk/training/online/courses/bioimage-archive-quick-tour"
     return urlType === "submission"? `${quickTourURL}/submitting-data-to-bioimage-archive-2/submission/` : quickTourURL
 }
+
+export async function asyncPool(limit, array, iteratorFn) {
+  const ret = [];
+  const executing = [];
+
+  for (const item of array) {
+    const p = Promise.resolve().then(() => iteratorFn(item));
+    ret.push(p);
+
+    if (limit <= array.length) {
+      const e = p.then(() => executing.splice(executing.indexOf(e), 1));
+      executing.push(e);
+      if (executing.length >= limit) {
+        await Promise.race(executing);
+      }
+    }
+  }
+
+  return Promise.all(ret);
+}
